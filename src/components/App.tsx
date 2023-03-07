6b496b6849b5f1d97f9e162560e9238dfd54bb67
Koushik Dey <koushik.dey@live.com>
2023-03-08 01:57:48 +0200

    connected component to redux

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Todo, fetchTodos } from '../actions';
+import { StoreState } from '../reducers';
 
-const App = () => {
-  return (
-    <div>It's start</div>
-  );
+interface AppProps {
+  todos: Todo[];
+  fetchTodos(): any;
 }
 
-export default App;
+class _App extends React.Component<AppProps> {
+  render() {
+    return <div>Hi there!</div>;
+  }
+}
+
+const mapStateToProps = ({ todos }: StoreState): { todos: Todo[] } => {
+  return { todos };
+};
+
+export const App = connect(
+  mapStateToProps,
+  { fetchTodos }
+)(_App);
+
+
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/App';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import { App } from './components/App';
 import { reducers } from './reducers';
 
 const root = ReactDOM.createRoot(
@@ -15,5 +15,5 @@ const store = createStore(reducers, applyMiddleware(thunk));
 root.render(
   <Provider store={store}>
     <App />
-  </Provider>
+  </Provider>,
 );
