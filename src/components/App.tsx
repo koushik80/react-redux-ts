28894d3ff4a38d8a1fd5fdebaf2bf4cf57ad96c2
Koushik Dey <koushik.dey@live.com>
2023-03-08 00:06:33 +0200

    initial setup

---
+++ src/components/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const App = () => {
+  return (
+    <div>It's start</div>
+  );
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './components/App';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { reducers } from './reducers';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const store = createStore(reducers, applyMiddleware(thunk));
+
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
